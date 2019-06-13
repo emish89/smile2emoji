d0a0895b75ac928b8f30278b7ef204d0b78bf795
federico <federico.ballarini@deltatre.com>
2019-06-13 17:52:44 +0200

    first commit

---
+++ test/index.js
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+import { emojiMap, checkText } from '../src';
+
+describe('test package functions', () => {
+  it('should test emoji map', () => {
+    const expectedVal = '👋';
+    assert(emojiMap['o/'] === expectedVal, 'invalid value :(');
+  });
+
+  it('should test awesome function', () => {
+    const expectedVal = 'ciao 👋';
+    assert(checkText('ciao o/') === expectedVal, 'invalid value :(');
+  });
+});
