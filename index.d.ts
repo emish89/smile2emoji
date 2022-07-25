2df5f80d6fe429ddb5d2db5b56bad6e8983851aa
Federico Ballarini <federico.ballarini.sc@gmail.com>
2022-07-25 22:59:54 +0200

    feat(index.d.ts): add exported functions

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,6 @@
 export function checkText(text: string): string;
+export function fromUnicodeToEmoji(text: string): string;
+export function checkTextWithAutoSuggestionsAndUnicode(text: string): string;
+export function checkTextWithAutoSuggestions(text: string): string;
 export const emojiMap: { [key: string]: string };
+export const mapStringToUnicode: { [key: string]: string };
